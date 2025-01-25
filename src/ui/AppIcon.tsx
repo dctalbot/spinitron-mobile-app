40555e67a4a43a392be30998c2ae9ebadec53cfd
dctalbot <dave.c.talbot@gmail.com>
2025-01-25 16:45:50 -0500

    eslint 9

--- src/ui/AppIcon.tsx
+++ src/ui/AppIcon.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { Ionicons } from "@expo/vector-icons"; // eslint-disable-line no-restricted-imports
+import { Ionicons } from "@expo/vector-icons";
 import { ComponentProps } from "react";
 import { useTheme } from "../theme/useTheme";
 
-export interface AppIconProps extends ComponentProps<typeof Ionicons> {}
+export type AppIconProps = ComponentProps<typeof Ionicons>;
 
 export function AppIcon(props: AppIconProps) {
   const theme = useTheme();
