b5ff0c509c0eb62e9c9086d3cafc3c635323dc62
dctalbot <dave.c.talbot@gmail.com>
2025-09-21 23:09:50 -0400

    build

--- cloudflare-worker/worker.js
+++ cloudflare-worker/worker.js
@@ -12,6 +12,6 @@ export default {
   async fetch(request, env, ctx) {
     // You can view your logs in the Observability dashboard
     console.info({ message: "Hello World Worker received a request!" });
-    return new Response("Hello World!!!!!");
+    return new Response("Hello World!!!!!!");
   },
 };
