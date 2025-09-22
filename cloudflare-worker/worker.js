c9e5e189aba992b3db6ac4918004f07c59977a91
dctalbot <dave.c.talbot@gmail.com>
2025-09-21 22:46:50 -0400

    init cloduflare

---
+++ cloudflare-worker/worker.js
@@ -0,0 +1,17 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    // You can view your logs in the Observability dashboard
+    console.info({ message: "Hello World Worker received a request!" });
+    return new Response("Hello World!!");
+  },
+};
