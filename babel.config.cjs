967c29c1628a8b7390671e791b84b2054a8edb36
dctalbot <dave.c.talbot@gmail.com>
2025-11-01 16:46:16 -0400

    removes logging in prod

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,5 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins:
+      process.env.NODE_ENV === "production" ? ["transform-remove-console"] : [],
   };
 };
